a3c025cec1b6fe21efc92ea7497af6508e410bc6
muhammad-jiku <muhammadjiku364@gmail.com>
2022-02-17 18:05:48 +0600

    function added

--- js/index.js
+++ js/index.js
@@ -1,35 +1,62 @@
-// expense section starts
+function getInputValue(money) {
+  const inputValue = document.getElementById(money + '-amount');
+  const inputValueAmount = parseInt(inputValue.value);
+  return inputValueAmount;
+}
+
+function expenseCalculation() {
+  const incomeAmount = getInputValue('income');
+  const foodExpenseAmount = getInputValue('food-expense');
+  const clothesExpenseAmount = getInputValue('cloth-expense');
+  const rentExpenseAmount = getInputValue('rent-expense');
+
+  const totalExpenseAmount =
+    foodExpenseAmount + clothesExpenseAmount + rentExpenseAmount;
+  const totalExpenseAmountText = document.getElementById(
+    'expense-amount-total'
+  );
+  const balanceAmount = incomeAmount - totalExpenseAmount;
+  const balanceAmountText = document.getElementById('balance-amount');
+
+  if (incomeAmount < totalExpenseAmount) {
+    totalExpenseAmountText.innerText =
+      'You can not spend more money than you earn!';
+    balanceAmountText.innerText = 'You can not spend more money than you earn!';
+  } else {
+    totalExpenseAmountText.innerText = totalExpenseAmount;
+    balanceAmountText.innerText = balanceAmount;
+
+    return balanceAmount;
+  }
+}
+
+function saveAndRemainBalanceCalculation() {
+  const incomeAmount = getInputValue('income');
+  const savingAmount = getInputValue('saving');
+  const totalBalance = expenseCalculation();
+
+  const totalSave = incomeAmount * (savingAmount / 100);
+  const savingTotalText = document.getElementById('saving-amount-total');
+
+  const remainingBalanceText = document.getElementById(
+    'remaining-amount-total'
+  );
+
+  if (totalSave > totalBalance) {
+    savingTotalText.innerText = 'You can not save more money than you have!';
+    remainingBalanceText.innerText = 'You need money to save!';
+  } else {
+    savingTotalText.innerText = totalSave;
+    const remainingBalance = totalBalance - totalSave;
+
+    remainingBalanceText.innerText = remainingBalance;
+  }
+}
+
 document.getElementById('calculate-btn').addEventListener('click', function () {
-  const incomeInput = document.getElementById('income-field');
-  const incomeInputAmount = parseInt(incomeInput.value);
-  const foodInput = document.getElementById('food-expense-field');
-  const foodInputAmount = parseInt(foodInput.value);
-  const clothInput = document.getElementById('cloth-expense-field');
-  const clothInputAmount = parseInt(clothInput.value);
-  const rentInput = document.getElementById('rent-expense-field');
-  const rentInputAmount = parseInt(rentInput.value);
-
-  const totalExpense = document.getElementById('total-expense');
-  totalExpense.innerText = foodInputAmount + clothInputAmount + rentInputAmount;
-  //   foodInput.value = '';
-  //   clothInput.value = '';
-  //   rentInput.value = '';
-
-  const balanceAmount = document.getElementById('total-balance');
-  balanceAmount.innerText = incomeInputAmount - totalExpense.innerText;
+  expenseCalculation();
 });
 
-// saving section starts
 document.getElementById('save-btn').addEventListener('click', function () {
-  const savingInput = document.getElementById('save-field');
-  //   savingInputAmount = parseInt(savingInput.value);
-
-  const balanceAmount = document.getElementById('total-balance');
-  const savingTotal = document.getElementById('total-saving');
-  savingTotal.innerText =
-    balanceAmount.innerText * (parseInt(savingInput.value) / 100);
-  //   console.log(balance);
-
-  const remainingAmount = document.getElementById('remaining-balance');
-  remainingAmount.innerText = balanceAmount.innerText - savingTotal.innerText;
+  saveAndRemainBalanceCalculation();
 });
